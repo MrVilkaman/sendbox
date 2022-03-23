645489959b97c7464cac87d6de3072ad0891e8e4
zahar <zahar.zolotarev@gmail.com>
2022-03-23 19:45:28 +0300

    добавил логгирование

--- app_currency/src/main/java/ru/zolotarev/tcurrency/di/DIContainer.kt
+++ app_currency/src/main/java/ru/zolotarev/tcurrency/di/DIContainer.kt
@@ -1,5 +1,7 @@
 package ru.zolotarev.tcurrency.di
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
@@ -8,10 +10,17 @@ import ru.zolotarev.tcurrency.data.network.CurrencyApi
 
 object DIContainer {
 
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            setLevel(HttpLoggingInterceptor.Level.BODY)
+        })
+        .build()
+
 
     val retrofit = Retrofit.Builder()
         .baseUrl("https://api.tinkoff.ru")
         .addConverterFactory(MoshiConverterFactory.create())
+        .client(client)
         .build()
         .create<CurrencyApi>()
 }
